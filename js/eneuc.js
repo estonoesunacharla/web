57a3262c9b33c8908f2dca2b6cac9c5d0e532395
Javier Garmón <jgarmon@inevio.com>
2018-04-08 12:49:26 +0200

    Fixed JS

--- js/eneuc.js
+++ js/eneuc.js
@@ -1,8 +1,8 @@
 $( function(){
   
-  function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  $('header').css('background','url("http://estonoesunacharla.com/img/bg/' + getRandomArbitrary(1,3) + '.jpg")')
+  $('header').css('background','url("http://estonoesunacharla.com/img/bg/' + getRandomInt(1,3) + '.jpg")')
 })
